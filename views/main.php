b86a268b98b3b126250860ed5fd6078aa6d10046
joelluong <luongdaitrungduong@gmail.com>
2018-11-10 16:12:58 +1100

    Edit logo image size

--- views/main.php
+++ views/main.php
@@ -11,7 +11,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark">
             <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">
-                <img src="../assets/img/share.png" />
+                <img src="../assets/img/share.png" width="48" height="48" />
                 <strong>Share Board</strong>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
